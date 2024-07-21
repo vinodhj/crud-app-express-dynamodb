8b22ae0e95dde1056ba7cf93edb95c2a3041338f
Vinodh <vinodh.jeevanantham@gmail.com>
2024-07-21 15:12:24 +0530

    batch update

---
+++ src/controllers/batch_update_product.controller.ts
@@ -0,0 +1,68 @@
+import {
+  TransactWriteItemsCommand,
+  UpdateItemCommand,
+  type TransactWriteItemsCommandInput,
+  type UpdateItemCommandInput,
+} from "@aws-sdk/client-dynamodb";
+import { getDynamoDBConfig } from "../config";
+import type { Request, Response } from "express";
+import _ from "lodash";
+
+// DynamoDB client
+const client_ddb = getDynamoDBConfig();
+
+// Batch size
+const batch_size = 25; // DynamoDB supports up to 25 items per request
+
+export const batchUpdateProducts = async (req: Request, res: Response) => {
+  const { input } = req.body;
+  const chunks = _.chunk(input, batch_size);
+  try {
+    for (const chunk of chunks) {
+      const updateItems = chunk.map((item: any) => {
+        const { pk, name, price, image } = item;
+        return {
+          Update: {
+            ExpressionAttributeNames: {
+              "#NAME": "name",
+              "#PRICE": "price",
+              "#UPDATED_AT": "updated_at",
+              ...(image && { "#IMAGE": "image" }),
+            },
+            ExpressionAttributeValues: {
+              ":name": { S: name },
+              ":price": { N: price.toString() },
+              ":updated_at": { S: new Date().toISOString() },
+              ...(image && { ":image": { S: image } }),
+            },
+            Key: {
+              PK: { S: pk },
+              SK: { S: "products" },
+            },
+            ReturnValues: "UPDATED_NEW",
+            TableName: process.env.TABLE_NAME as string,
+            ...(image && {
+              UpdateExpression:
+                "SET #NAME = :name, #PRICE = :price, #IMAGE = :image, #UPDATED_AT = :updated_at",
+            }),
+            ...(!image && {
+              UpdateExpression:
+                "SET #NAME = :name, #PRICE = :price, #UPDATED_AT = :updated_at",
+            }),
+          },
+        };
+      });
+
+      const params: TransactWriteItemsCommandInput = {
+        TransactItems: updateItems,
+      };
+      const command = new TransactWriteItemsCommand(params);
+      const data = await client_ddb.send(command);
+      console.log("Item updated successfully: " + JSON.stringify(data));
+    }
+    res.send("Item updated successfully : " + chunks.length);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send(error);
+  }
+};
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,18 +4,19 @@ import { updateProduct } from "../controllers/update_product.controller.ts";
 import { product } from "../controllers/product.controller.ts";
 import { allProducts } from "../controllers/all_products.controller.ts";
 import { deleteProduct } from "../controllers/delete_product.controller.ts";
+import { batchUpdateProducts } from "../controllers/batch_update_product.controller.ts";
 
 const router = express.Router();
 
-// create a product
+// Routes
 router.post("/post-products", createProduct);
-
 router.put("/update-product", updateProduct);
-
 router.get("/product/:pk", product);
-
 router.get("/allProducts", allProducts);
-
 router.delete("/delete-product/:pk", deleteProduct);
+
+// Batch operations
+router.put('/batch-update-products', batchUpdateProducts);
+
 
 export default router;
