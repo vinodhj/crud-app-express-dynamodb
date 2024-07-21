f4e8bcad3f6eeffe738be18504333f0d29f64dc5
Vinodh <vinodh.jeevanantham@gmail.com>
2024-07-21 12:29:43 +0530

    getByPk

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,34 @@
+import {
+  GetItemCommand,
+  type GetItemCommandInput,
+} from "@aws-sdk/client-dynamodb";
+import { getDynamoDBConfig } from "../config";
+import type { Request, Response } from "express";
+
+// DynamoDB client
+const client_ddb = getDynamoDBConfig();
+
+export const product = async (req: Request, res: Response) => {
+  const { pk } = req.params;
+  // Prepare item data for DynamoDB
+  const params: GetItemCommandInput = {
+    Key: {
+      PK: { S: pk },
+      SK: { S: "products" },
+    },
+    TableName: process.env.TABLE_NAME as string,
+  };
+  //console.log(params);
+  try {
+    // Insert into DynamoDB
+    const command = new GetItemCommand(params);
+    const data = await client_ddb.send(command);
+    console.log(JSON.stringify(data));
+    if (!data.Item) return res.status(404).send("Item not found");
+    res.send("Item fetched successfully: " + JSON.stringify(data.Item));
+    // res.send();
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send(error);
+  }
+};
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createProduct } from "../controllers/create_product.controller.ts";
 import { updateProduct } from "../controllers/update_product.controller.ts";
+import { product } from "../controllers/product.controller.ts";
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ const router = express.Router();
 router.post("/post-products", createProduct);
 
 router.put("/update-product", updateProduct);
+
+router.get("/product/:pk", product);
 
 export default router;
